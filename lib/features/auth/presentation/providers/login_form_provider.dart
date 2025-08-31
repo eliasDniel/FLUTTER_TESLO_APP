121960035bb4db490fe6e46986d71f40f7cdc924
eliasDniel <eramosg2@unemi.edu.ec>
2025-08-31 12:12:22 -0500

    configuraciones y el estado de el formulario creados

---
+++ lib/features/auth/presentation/providers/login_form_provider.dart
@@ -0,0 +1,19 @@
+// ! 1. ESTADO DEL PROVIDER
+
+import 'package:flutter_teslo_app/features/shared/shared.dart';
+class LoginFormProvider {
+  final bool isPosting;
+  final bool isFormPosted;
+  final bool isValid;
+  final Email email;
+  final Password password;
+
+  LoginFormProvider({
+    this.isPosting = false, 
+    this.isFormPosted = false, 
+    this.isValid = false, 
+    this.email = const Email.pure(), 
+    this.password = const Password.pure()
+  });
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'config/config.dart';
 import 'config/router/app_router.dart';
 
-
 void main() {
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MainApp()));
 }
 
 class MainApp extends StatelessWidget {
